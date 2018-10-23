0e139af4f24c4f19863605e5de3541dd26480c93
markelcortazar <43407435+markelcortazar@users.noreply.github.com>
2018-10-23 10:09:31 +0200

    Update App.java
    Newest version of App, utilizing the changes in CliApliBase

--- App.java
+++ App.java
@@ -1,7 +1,10 @@
 package project;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.naming.directory.InvalidAttributesException;
 
 import dsa.CliApliBase;
 import dsa.CliApliBase.Command;
@@ -21,6 +24,7 @@ public class App {
 		while (true) {
 			System.out.println(toAsk);
 			String answer = sc.next();
+			sc.nextLine();
 			if (answer.toLowerCase().equals("y") || answer.toLowerCase().equals("yes")
 					|| answer.toLowerCase().equals("confirm")) {
 				return true;
@@ -34,18 +38,9 @@ public class App {
 	}
 
 	// Functions called via the cli commands
-	static Command readUsers = () -> {
-		try {
-			project.ReadFile.readUsers(inFilePath);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Users added");
-	};
-	static Command saveUsers = () -> {
-		System.out.println("Users saved.");
-	};
+	// Sets the value of inFilePath
 	static Command setRead = () -> {
+		System.out.println("Please introduce the desired path to read from.");
 		String newPath = sc.next();
 		if (askYesNo("Is " + newPath + " the path you want to set as the read path?")) {
 			inFilePath = newPath;
@@ -53,7 +48,10 @@ public class App {
 		} else
 			System.out.println("File path to read not updated.");
 	};
+	// Sets the value of outFilePath
 	static Command setWrite = () -> {
+
+		System.out.println("Please introduce the desired path to write to.");
 		String newPath = sc.next();
 		if (askYesNo("Is " + newPath + " the path you want to set as the write path?")) {
 			outFilePath = newPath;
@@ -61,26 +59,100 @@ public class App {
 		} else
 			System.out.println("File path to write not updated.");
 	};
+	// Reads users from inFilePath
+	static Command readUsers = () -> {
+		try {
+			System.out.println(UserMap.addUsers(project.ReadFile.readUsers(inFilePath)) + " users added");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	};
+	// Reads friendships from inFilePath
+	static Command readFriendships = () -> {
+		try {
+			System.out.println(FriendshipMap.addFriendship(ReadFile.readFriends(inFilePath)) + " friendships added");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	};
+	// Prints users to outFilePath
+	static Command saveUsers = () -> {
+		try {
+			WriteFile.write(outFilePath, UserMap.print());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Users saved to " + outFilePath + ".");
+	};
+	// Prints friendships to outFilePath
+	static Command saveFriends = () -> {
+		try {
+			WriteFile.write(outFilePath, FriendshipMap.print());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Friends saved to " + outFilePath + ".");
+	};
+	// Searches for users containing a given value in the specified field and prints
+	// them to stdout
 	static Command search = () -> {
-		System.out.println("This are the users that match");
+		System.out.println(
+				"Please introduce the name of the field you are searching :\n id, groupcode, name, surname, birthplace, home, gender, birthday, studiedAt, workplaces, movies");
+		String field = sc.next();
+		sc.nextLine();
+		System.out.println("Please introduce the value you are searching:");
+		String value = sc.next();
+		sc.nextLine();
+		ArrayList<User> results = new ArrayList<>();
+		try {
+			results = UserMap.findValue(field, value);
+		} catch (InvalidAttributesException e) {
+			e.printStackTrace();
+		}
+		if (!results.isEmpty()) {
+			System.out.println("This are the " + results.size() + " that match:");
+			for (User u : results) {
+				if (u != null)
+					System.out.println(u.toString());
+			}
+			System.out.println("\n");
+		} else
+			System.out.println("No users match.\n");
+	};
+	// Prints users to stdout
+	static Command printUsers = () -> {
+		System.out.println(UserMap.print() + "\n");
+	};
+	// Prints friendships to stdout
+	static Command printFriends = () -> {
+		System.out.println(FriendshipMap.print() + "\n");
 	};
 
 	// App configuration variables
 	// Names for the accepted commands
-	static String[] commandNames = { "readUsers", "saveUsers", "setRead", "setWrite", "search" };
+	static String[] commandNames = { "readUsers", "readFriends", "saveUsers", "saveFriends", "setRead", "setWrite",
+			"search", "printUsers", "printFriends" };
 	// Test to display to user explaining options
-	static String[] options = { "Read users from file", "Save users to a file", "Set read file path",
-			"Set write file path", "Search users" };
+	static String[] options = { "Read users from file", "Read friendship relationships from file",
+			"Save users to a file", "Save friendships to a file", "Set read file path", "Set write file path",
+			"Search users", "Displays all user data", "Displays all friendship relationships." };
 	// Functions to be called upon command execution
-	static Command[] test = { readUsers, saveUsers, setRead, setWrite, search };
+	static Command[] test = { readUsers, readFriendships, saveUsers, saveFriends, setRead, setWrite, search, printUsers,
+			printFriends };
 	// App instance declaration
 	static CliApliBase app;
 
 	public static void main(String[] args) throws Exception {
+		// Initialize app
 		app = new CliApliBase(commandNames, test, options);
+		// Set setRead to be executed at the start by default
+		app.setInitAction(setRead);
+		// Set the initial message of the aplication
+		app.setStartMessage(
+				"Welcome to the programming project for DSA made by the group G611837. This program emulates a social network.\n"
+						+ "-------------------------------------------------------------------------------------------------------------");
 		// Main loop
-		while (!app.isFinished())
-			app.readUserInput();
+		app.startReading();
 
 		sc.close();
 	}
